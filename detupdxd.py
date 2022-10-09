6de5ae340365962061a8c53c9e1372c9e5c44a08
_pravakar <61041599+bigOcodes@users.noreply.github.com>
2022-10-09 10:56:40 +0530

    Update Big Sum Program

--- detupdxd.py
+++ detupdxd.py
@@ -1,3 +1,15 @@
 #Creating a new file
 print("New")
 print("Jeevan jyoti Sahoo")
+
+# Biggest Sum
+
+handler = open("bigsum.txt")
+
+lines = handler.readlines()
+ar = lines[1].split(' ')
+
+for i in range(len(ar)):
+    ar[i] = int(ar[i])
+    
+print(sum(ar))
